e08610dd8677bc9b8d911cee0803c60e2414dcd2
mitchya1 <mitchell.anderson@colorado.edu>
2019-01-31 11:26:19 -0700

    add best practice checker

---
+++ bhi_pw/best_practice_checker/main_func.py
@@ -0,0 +1,34 @@
+
+import re, json
+def check_best_practices(password):
+    letter_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    character_list = "@#$%^&*()_+="
+    number_list = "1234567890"
+
+    json_response = {}
+
+    json_response['pw_length'] = len(password)
+    
+    if len(password) < 10:
+        json_response['length'] = "bad"
+    else:
+        json_response['length'] = "ok"
+
+    if re.findall('([A-Z])', password):
+        json_response['uppercase'] = "ok"
+    else:
+        json_response['uppercase'] = "bad"
+    
+    if re.findall('([1-9])', password):
+        json_response['number'] = "ok"
+    else:
+        json_response['number'] = "bad"
+
+    for character in character_list:
+        if character in password.strip().split():
+            json_response['special_char'] = "ok"
+        else:
+            json_response['special_char'] = "bad"
+
+
+    return json.dumps(json_response)
