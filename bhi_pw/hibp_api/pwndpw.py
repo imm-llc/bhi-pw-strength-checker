974dac823d82a6bc6943fab8ccebe1684dfd0adb
mitchya1 <mitchell.anderson@colorado.edu>
2019-01-31 11:26:01 -0700

    update

--- bhi_pw/hibp_api/pwndpw.py
+++ bhi_pw/hibp_api/pwndpw.py
@@ -3,30 +3,29 @@ import requests, json, hashlib
 from sys import argv
 # Tool to check PW hashes against Pwned Passwords
 
-PW = argv[1]
-PW = PW.encode('utf-8')
+def main_func(password):
+    
 
-PWHASH = hashlib.sha1(PW).hexdigest().upper()
-PwndedHashPrefix = PWHASH[0:5]
-PwndedHashSuffix = PWHASH[5:]
+    password = password.encode('utf-8')
 
-
-print("Using PW Hash: {}".format(PwndedHashPrefix))
-
-HEADERS = {'User-Agent': 'BHI Pwned PW Checker'}
-
-r = requests.get("https://api.pwnedpasswords.com/range/"+PwndedHashPrefix, headers=HEADERS)
-
-if r.status_code == 200:
-    print("Success Searching Pwned Passwords Database")
-else:
-    print("Error Searching Pwned Passwords Database")
-
-for line in r.text.splitlines():
-    hashSuffix, count = line.split(':')
-
-    if hashSuffix == PwndedHashSuffix:
-        print("Your password has been breached {} times".format(str(count)))
+    PWHASH = hashlib.sha1(password).hexdigest().upper()
+    PwndedHashPrefix = PWHASH[0:5]
+    PwndedHashSuffix = PWHASH[5:]
 
 
+    HEADERS = {'User-Agent': 'BHI Pwned PW Checker'}
+
+    r = requests.get("https://api.pwnedpasswords.com/range/"+PwndedHashPrefix, headers=HEADERS)
+
+    if r.status_code != 200:
+        return "There was an error checking this password against Have I Been Pwned"
+
+
+    for line in r.text.splitlines():
+        hashSuffix, count = line.split(':')
+
+        if hashSuffix == PwndedHashSuffix:
+            return str(count)
+        else:
+            return 0
 
--- bhi_pw/routes.py
+++ bhi_pw/routes.py
@@ -1,8 +1,53 @@
 from bhi_pw import app
-
+from bhi_pw.hibp_api import pwndpw
+from bhi_pw.best_practice_checker import main_func
 from flask import render_template, request
 import json, requests, binascii
 
-@app.route('/')
-def root():
-    return render_template("base.html")
+@app.route('/', methods=["POST", "GET"])
+def homepage():
+    if request.method == "POST":
+        password = request.form.get('Password')
+        pwned_password_count = pwndpw.main_func(str(password))
+        print(pwned_password_count)
+        pwned_password_count = int(pwned_password_count)
+        if pwned_password_count > 0:
+            pwned_message = "Your password has been found in {} breaches".format(str(pwned_password_count))
+        else:
+            pwned_message = "This password was not found in any breaches"
+
+
+        best_practice_match = main_func.check_best_practices(password)
+        json_best_practice = json.loads(best_practice_match)
+        
+        best_practice_result = {}
+        
+        if json_best_practice['length'] == "ok":
+            best_practice_result['length'] = "Your password is long. Great!"
+        else:
+            best_practice_result['length'] = "Your password is only {} characters. You should use a password that contains at least 10 characters".format(str(json_best_practice['pw_length']))
+        
+        if json_best_practice['uppercase'] == "bad":
+            best_practice_result['uppercase'] = "You should include at least one upper case character in your password"
+        else:
+            best_practice_result['uppercase'] = "You have at least one uppercase character. Nice!"
+        
+        if json_best_practice['number'] == "bad":
+            best_practice_result['number'] = "You should include at least one number in your password"
+        else:
+            best_practice_result['number'] = "You have at least one number in your password. Sweet!"
+        
+        if json_best_practice['special_char'] == "bad":
+            best_practice_result['special_char'] = "You do not have a special character in your password. Special characters include: {}".format("@#$%^&*()_+=")
+        else:
+            best_practice_result['special_char'] = "You have a special character in your password. Awesome!"
+        
+
+
+        return render_template('result.html', breach_message=pwned_message, best_practice_result=best_practice_result)
+
+    return render_template("home.html")
+
+@app.route('/about', methods=["GET"])
+def about():
+    return "hello world"
